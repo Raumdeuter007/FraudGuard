987b318242b127a17edf8e719f4f3521dbc41e98
Luqman PC <luqmanarshad05@gmail.com>
2026-05-03 04:49:52 +0500

    Frontend Section added in Readme file

--- Backend/src/auth/schemas.py
+++ Backend/src/auth/schemas.py
@@ -1,5 +1,5 @@
 import uuid
-from pydantic import EmailStr
+from pydantic import EmailStr, Field
 from fastapi_users import schemas
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
@@ -7,8 +7,8 @@ class UserRead(schemas.BaseUser[uuid.UUID]):
 
 class UserCreate(schemas.CreateUpdateDictModel):
     email: EmailStr
-    password: str
-    name: str
+    password: str = Field(min_length=8, max_length=128)
+    name: str = Field(min_length=1, max_length=50)
 
 class UserUpdate(schemas.BaseUserUpdate):
     name: str
